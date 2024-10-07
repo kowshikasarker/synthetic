7a3b90202ede8ee261168199b57efb6ad7d58acb
Kowshika Sarker <63386569+kowshikasarker@users.noreply.github.com>
2024-10-07 14:59:18 -0500

    Update loss.py

--- src/loss.py
+++ src/loss.py
@@ -38,23 +38,16 @@ class BCELoss(torch.nn.Module):
         print('z', z.shape, 'edge_index', edge_index.shape, 'bce_loss', bce_loss.shape)
         return bce_loss
     
-class MultiLossEarlyStopping(Callback):
+'''class MultiLossEarlyStopping(Callback):
     def __init__(self, monitor, min_delta, patience, mode):
-        print('monitor', monitor)
-        print('min_delta', min_delta)
-        print('patience', patience)
-        print('mode', mode)
         super().__init__()
         self.mode_dict = {"min": torch.lt, "max": torch.gt}
         self.monitor = monitor
-        
-        for m in mode:
-            if m not in self.mode_dict:
-                raise Exception("uncrecognized mode", m)
                 
         self.min_delta = dict(zip(monitor, min_delta))
         self.patience = dict(zip(monitor, patience))
         self.mode = dict(zip(monitor, mode))
+        #self.check_finite = dict(zip(monitor, check_finite))
         
         self.best_score = {}
         self.wait_count = {}
@@ -66,33 +59,93 @@ class MultiLossEarlyStopping(Callback):
             self.min_delta[m] *= 1 if monitor_op == torch.gt else -1
             self.best_score[m] = torch_inf if monitor_op == torch.lt else -torch_inf
             self.wait_count[m] = 0
-            
-    def validate_condition_metric(self, logs):
-        for m in self.monitor:
-            monitor_val = logs.get(m)
-            if (monitor_val is None):
-                raise RuntimeError("Early stopping metric " + m + " absent in logs.")
-                return False
-        return True
     
     @override
     def on_validation_end(self, trainer, pl_module):
         logs = trainer.callback_metrics
+        print('logs', logs, end='\n')
+        
         should_stop_count = 0
         for m in self.monitor:
             current = logs[m].squeeze()
+            if(self.check_finite[m]):
+                if not (torch.isfinite(current)):
+                    print("Should stop for", m, "(not finite)")
+                    should_stop_count += 1
+                    continue
             monitor_op = self.mode_dict[self.mode[m]]
+            print('current', current, 'best', self.best_score[m], 'monitor_op', monitor_op)
+            
             if monitor_op(current - self.min_delta[m], self.best_score[m].to(current.device)):
                 self.best_score[m] = current
                 self.wait_count[m] = 0
             else:
                 self.wait_count[m] += 1
+                print('self.wait_count', self.wait_count)
                 if self.wait_count[m] >= self.patience[m]:
-                    print("Should stop for", m)
+                    print("Should stop for", m, " (not improved)")
                     should_stop_count += 1
         print("should_stop_count", should_stop_count)
         should_stop = should_stop_count == len(self.monitor)
-        
+        print('should_stop', should_stop)
+        print('trainer.should_stop', trainer.should_stop)
         trainer.should_stop = trainer.should_stop or should_stop
+        print('trainer.should_stop', trainer.should_stop)
+        if should_stop:
+            self.stopped_epoch = trainer.current_epoch'''
+    
+class MultiLossEarlyStopping(Callback):
+    def __init__(self, monitor, min_delta, patience, mode, check_finite):
+        super().__init__()
+        self.mode_dict = {"min": torch.lt, "max": torch.gt}
+        self.monitor = monitor
+                
+        self.min_delta = dict(zip(monitor, min_delta))
+        self.patience = dict(zip(monitor, patience))
+        self.mode = dict(zip(monitor, mode))
+        self.check_finite = dict(zip(monitor, check_finite))
+        
+        self.best_score = {}
+        self.wait_count = {}
+        
+        torch_inf = torch.tensor(torch.inf)
+        
+        for m in self.monitor:
+            monitor_op = self.mode_dict[self.mode[m]]
+            self.min_delta[m] *= 1 if monitor_op == torch.gt else -1
+            self.best_score[m] = torch_inf if monitor_op == torch.lt else -torch_inf
+            self.wait_count[m] = 0
+    
+    @override
+    def on_validation_end(self, trainer, pl_module):
+        logs = trainer.callback_metrics
+        print('logs', logs, end='\n')
+        
+        should_stop_count = 0
+        for m in self.monitor:
+            current = logs[m].squeeze()
+            if(self.check_finite[m]):
+                if not (torch.isfinite(current)):
+                    print("Should stop for", m, "(not finite)")
+                    should_stop_count += 1
+                    continue
+            monitor_op = self.mode_dict[self.mode[m]]
+            print('current', current, 'best', self.best_score[m], 'monitor_op', monitor_op)
+            
+            if monitor_op(current - self.min_delta[m], self.best_score[m].to(current.device)):
+                self.best_score[m] = current
+                self.wait_count[m] = 0
+            else:
+                self.wait_count[m] += 1
+                print('self.wait_count', self.wait_count)
+                if self.wait_count[m] >= self.patience[m]:
+                    print("Should stop for", m, " (not improved)")
+                    should_stop_count += 1
+        print("should_stop_count", should_stop_count)
+        should_stop = should_stop_count == len(self.monitor)
+        print('should_stop', should_stop)
+        print('trainer.should_stop', trainer.should_stop)
+        trainer.should_stop = trainer.should_stop or should_stop
+        print('trainer.should_stop', trainer.should_stop)
         if should_stop:
             self.stopped_epoch = trainer.current_epoch
